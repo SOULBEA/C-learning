a3976ba268d73349eae215d64aaff2fc37da2555
snorlax_codes <siarpit119@gmail.com>
2024-05-21 15:52:05 +0530

    day_2

--- day2.c
+++ day2.c
@@ -1,19 +1,21 @@
 // VARIABLES : VARIABLE IS THE NAME OF A MEMEORY LOCATION WHICH STORES SOME DATA
-
 #include<stdio.h>
 
 int main(){
-    int a = 2; // variable a is an interger type variable.
-    float b = 3.0; // varable b is an real const.
-    char c = 'A'; // variable c is an character const.
-    printf("value of a = %d\n", a);
-    printf("value of b = %f\n", b);
-    printf("value of c = %d\n", c);
+    int _a =2;
+    float bye_hi = 3.1415;
+    char c = 'A';
+    printf("value of a = %d \n", _a);
+    printf("value of b = %f \n", bye_);
+    // printf("value of c = %c", c);
+
     return 0;
 }
-
 // RULSE FOR WRITING OR ASSING VARAIBLES:
 // 1. FIRST CHARACTER MUST BE '_' OR AN ALPHABET.
 // 2. NO COMMAS AND NO BLANK SPACES ARE ALLOWED.
 // 3. NO SPECIAL CHARACTER ARE ALLOWED OTHER THAN '_'.
 // 4. vARIABLE MUST BE CASE SENSITIVE
+
+
+
