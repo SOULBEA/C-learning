a5133705a9a4471ca14c2bec41324b951bc5394c
snorlax_codes <siarpit119@gmail.com>
2024-05-22 21:12:10 +0530

    day_4

---
+++ day4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int a = 2; // we added int in front of a variable because it was storing integer constant
+    float pi = 3.14; // we added float in front of pi variable because it was storing real constant
+    char A = 'A'; // we added char in front of 'A' varible because it was stroing character constant
+    printf("a = %d", a);
+    printf("pi = %f", pi);
+    printf("A = %c", A);
+    int x, y, z;
+    x= y = z = 4;
+    printf("the value of x %d \n", x);
+    printf("The value of y %d \n", y);
+    printf("the value of z %d \n ", z);
+    // we assign the value of xy and z as 4 as an integer constant and printed it
+
+    return 0;
+}
